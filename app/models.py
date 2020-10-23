0c633c8662f0d0a101d813d75f22fb52db2cd003
Joe Honeywood <joehoneywood@gmail.com>
2020-10-23 16:19:59 +0100

    init db

--- app/models.py
+++ app/models.py
@@ -1,46 +1,33 @@
 from datetime import datetime
 from app import db
 
-class Notebooks(db.model):
-    notebook_id = db.column(db.Integer, primary_key=True)
-    notebook_name = db.column(db.String(64), index=True, unique=True)
-    notebook_created_at_date = db.column(db.DateTime, index=True, default=datetime.utcnow)
+class Notebooks(db.Model):
+    notebook_id = db.Column(db.Integer, primary_key=True)
+    notebook_name = db.Column(db.String(64), index=True, unique=True)
+    notebook_created_at_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     chapters = db.relationship('Chapter', backref='notebook', lazy='dynamic')
 
     def __repr__(self):
         return '<Notebook {}>'.format(self.notebook_name)
 
-class Chapters(db.model):
-    chapter_id = db.column(db.Integer, primary_key=True)
-    notebook_id = db.column(db.Integer, db.ForeignKey('notebook.notebook_id'))
-    chapter_name = db.column(db.String(120))
-    chapter_created_at_date = db.column(db.DateTime, index=True, default=datetime.utcnow)
+class Chapters(db.Model):
+    chapter_id = db.Column(db.Integer, primary_key=True)
+    notebook_id = db.Column(db.Integer, db.ForeignKey('notebooks.notebook_id'))
+    chapter_name = db.Column(db.String(120))
+    chapter_created_at_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     notes = db.relationship('Note', backref='chapter', lazy='dynamic')
 
     def __repr__(self):
         return '<Chapter {}>'.format(self.chapter_name)
 
-class Notes(db.notes):
-    note_id = db.column(db.Integer, primary_key=True)
-    chapter_id = db.column(db.Integer, db.ForeignKey('chapters.chapter_id'))
-    notebook_id = db.column(db.Integer, db.ForeignKey('notebooks.notebook_id'))
-    note_name = db.column(db.String(120))
-    note_tags = db.column(db.String(120))
-    note_body = db.column(db.Text)
-    note_created_at_date = db.column(db.DateTime, index=True, default=datetime.utcnow)
+class Notes(db.Model):
+    note_id = db.Column(db.Integer, primary_key=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapters.chapter_id'))
+    notebook_id = db.Column(db.Integer, db.ForeignKey('notebooks.notebook_id'))
+    note_name = db.Column(db.String(120))
+    note_tags = db.Column(db.String(120))
+    note_body = db.Column(db.Text)
+    note_created_at_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Note {}>'.format(self.note_name)
-    
-    
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-
-    
